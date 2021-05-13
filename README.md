7f8e6cb9ca4583e0daa373873b933d214b4a52f0
harleyoconnor <harleyoc1@gmail.com>
2021-05-13 23:00:03 +0100

    Initial 1.16.5 port work.

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
-<img src="https://github.com/Harleyoc1/DynamicTreesVampirism/blob/master/banner.png?raw=true" alt="banner" width="1176"/>
-
-# Dynamic Trees for Vampirism
+![Banner](./banner.png)
+<h1 align="center">
+Dynamic Trees for Nature's Aura
+</h1>
+<p align="center">
+    <a href="https://www.curseforge.com/minecraft/mc-mods/dynamic-trees-vampirism">
+        <img src="http://cf.way2muchnoise.eu/dynamic-trees-vampirism.svg"  alt="Downloads"/>
+        <img src="http://cf.way2muchnoise.eu/versions/dynamic-trees-vampirism.svg"  alt="Versions"/>
+    </a>
+    <a href="https://www.curseforge.com/minecraft/mc-mods/dynamictrees">
+        <img src="http://cf.way2muchnoise.eu/title/dynamictrees_Requires_%20.svg"  alt="RequiresDT"/>
+    </a>
+</p>
+<br>
 Dynamic Trees compatibility add-on for Vampirism.
 
 ## Download
 Download at the [CurseForge project](https://www.curseforge.com/minecraft/mc-mods/dynamic-trees-vampirism), or at the [Releases](https://github.com/Harleyoc1/DynamicTreesVampirism/releases) section.
 
 ## Supported Trees
-- Spruce Oak
+- Vampire Spruce
+- Bloody Spruce
---
+++ src/main/java/com/harleyoconnor/dtvampirism/DTVampirism.java
@@ -0,0 +1,42 @@
+package com.harleyoconnor.dtvampirism;
+
+import com.ferreusveritas.dynamictrees.api.registry.RegistryHandler;
+import com.ferreusveritas.dynamictrees.blocks.rootyblocks.RootyBlock;
+import com.ferreusveritas.dynamictrees.systems.DirtHelper;
+import com.ferreusveritas.dynamictrees.systems.RootyBlockHelper;
+import net.minecraft.block.Block;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+
+/**
+ * @author Harley O'Connor
+ */
+@Mod(DTVampirism.MOD_ID)
+public final class DTVampirism {
+
+    public static final String MOD_ID = "dtvampirism";
+
+    public static final String CORRUPT_DIRT_LIKE = "corrupt_dirt_like";
+
+    public DTVampirism() {
+        RegistryHandler.setup(MOD_ID);
+
+        DirtHelper.createNewAdjective(CORRUPT_DIRT_LIKE);
+
+        FMLJavaModLoadingContext.get().getModEventBus().register(this);
+    }
+
+    @SubscribeEvent
+    public void onBlockRegistry(final RegistryEvent.Register<Block> event) {
+        final Block cursedEarth = ForgeRegistries.BLOCKS.getValue(new ResourceLocation("vampirism", "cursed_earth"));
+        DirtHelper.registerSoil(cursedEarth, CORRUPT_DIRT_LIKE);
+
+        for (final RootyBlock rooty : RootyBlockHelper.generateListForRegistry(true, MOD_ID))
+            event.getRegistry().register(rooty);
+    }
+
+}
