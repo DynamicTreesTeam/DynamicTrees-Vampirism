4d479dfc48b22ce072119d5f4a8d4906c2523140
Jacky <jzebrablanket@gmail.com>
2026-06-23 16:22:04 -0400

    Continued cleanup work
    Current bug: Can't runData without error from dark spurce leaves

--- src/main/java/com/dtteam/dtvampirism/DTVampirism.java
+++ src/main/java/com/dtteam/dtvampirism/DTVampirism.java
@@ -1,11 +1,20 @@
 package com.dtteam.dtvampirism;
 
+import com.dtteam.dynamictrees.block.fruit.Fruit;
+import com.dtteam.dynamictrees.block.leaves.LeavesProperties;
+import com.dtteam.dynamictrees.block.pod.Pod;
+import com.dtteam.dynamictrees.block.soil.SoilProperties;
+import com.dtteam.dynamictrees.data.GatherDataHelper;
+import com.dtteam.dynamictrees.tree.family.Family;
+import com.dtteam.dynamictrees.tree.species.Species;
 import com.mojang.logging.LogUtils;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
 import org.slf4j.Logger;
 
 @Mod(DTVampirism.MODID)
@@ -15,9 +24,7 @@ public class DTVampirism {
 
     public DTVampirism(IEventBus modEventBus, ModContainer modContainer) {
         modEventBus.addListener(this::commonSetup);
-        //NeoForge.EVENT_BUS.register(this);
-
-        DTVampirismRegistries.setup();
+        modEventBus.addListener(this::gatherData);
 
     }
 
@@ -27,6 +34,17 @@ public class DTVampirism {
 
     private void commonSetup(final FMLCommonSetupEvent event) {
 
+    }
+
+    private void gatherData(final GatherDataEvent event) {
+        GatherDataHelper.gatherAllData(MODID, event,
+                SoilProperties.REGISTRY,
+                Family.REGISTRY,
+                Species.REGISTRY,
+                LeavesProperties.REGISTRY,
+                //Fruit.REGISTRY,
+                Pod.REGISTRY
+        );
     }
 
 }
